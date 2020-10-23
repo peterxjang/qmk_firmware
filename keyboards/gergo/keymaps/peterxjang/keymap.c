3df13bc372856e1854feaf74270598c79a62682f
Peter Jang <peter.x.jang@gmail.com>
2020-10-23 13:56:40 -0500

    Fix base layer keys

--- keyboards/gergo/keymaps/peterxjang/keymap.c
+++ keyboards/gergo/keymaps/peterxjang/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | LShift |   Z  |   X  |   C  |   V  |   B  | LMB  |           |  }]  |   N  |   M  | ,  < | . >  | /  ? | Enter  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *                    .----------.   .-------.                                 .------.   .-----.
- *                    | Super    |   | BKSP  |                                 |Space |   | L1  |
+ *                    | Left CMD |   | BKSP  |                                 |Space |   | L1  |
  *                    '----------'   '-------'                                 `------.   '-----'
  *                                        ,-------.                      ,-------.
  *                                        | Vol+  |                      | Br+   |
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,                 KC_Q,  KC_W,   KC_E,   KC_R, KC_T,                                          KC_Y,    KC_U, KC_I, KC_O,   KC_P,              KC_PIPE,
     MT(MOD_LCTL, KC_ESC),   KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_EQL,                        KC_LBRC,  KC_H,    KC_J, KC_K, KC_L,   LT(MOVE, KC_SCLN), KC_QUOT,
     KC_LSFT,                KC_Z,  KC_X,   KC_C,   KC_V, KC_B, KC_MINS, KC_VOLU,     KC_BRIU, KC_RBRC,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,           KC_ENT,
-                                     MOD_LGUI,  KC_BSPC,  HYPR(KC_NO),  KC_VOLD,     KC_BRID, KC_LALT,  KC_ENT, LT(SYMB, KC_NO)
+                                      KC_LCMD,  KC_BSPC,  HYPR(KC_NO),  KC_VOLD,     KC_BRID, KC_LALT,  KC_SPC, LT(SYMB, KC_NO)
     ),
 /* Keymap 1: Symbols layer
  *
