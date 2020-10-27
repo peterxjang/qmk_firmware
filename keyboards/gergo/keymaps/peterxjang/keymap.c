5992c3b09cc28fafe154c7f71f0f801327f2baed
Peter Jang <peter.x.jang@gmail.com>
2020-10-27 15:54:31 -0500

    Add optional outer columns

--- keyboards/gergo/keymaps/peterxjang/keymap.c
+++ keyboards/gergo/keymaps/peterxjang/keymap.c
@@ -23,11 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,-------------------------------------------.                         ,-------------------------------------------.
- * |        |   Q  |   W  |   E  |   R  |   T  |                         |   Y  |   U  |   I  |   O  |   P  |        |
+ * |   Tab  |   Q  |   W  |   E  |   R  |   T  |                         |   Y  |   U  |   I  |   O  |   P  |   \ |  |
  * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |        |Ctrl/A|   S  |   D  |   F  |   G  | Esc  |           | Spc  |   H  |   J  |   K  |   L  | ;/L2 |        |
+ * |Ctrl/Esc|Ctrl/A|   S  |   D  |   F  |   G  | Esc  |           | Spc  |   H  |   J  |   K  |   L  | ;/L2 |  ' "   |
  * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * |        |Shft/Z|   X  |   C  |   V  |   B  | Bs   |           |      |   N  |   M  | ,  < | . >  |Shft/?|        |
+ * |  Shift |Shft/Z|   X  |   C  |   V  |   B  | Bs   |           | Tab  |   N  |   M  | ,  < | . >  |Shft/?|  Enter |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *                       .-------.   .------.                                 .------.   .-------.
  *                       |Opt/Esc|   |Cmd/Bs|                                 |L1/Spc|   |Opt/Tab|
@@ -41,15 +41,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------'                      `--------------'
  */
 [BASE] = LAYOUT_gergo(
-    KC_NO, KC_Q,         KC_W, KC_E, KC_R, KC_T,                                         KC_Y, KC_U, KC_I,    KC_O,   KC_P,              KC_NO,
-    KC_NO, LCTL_T(KC_A), KC_S, KC_D, KC_F, KC_G, KC_ESC,                        KC_SPC,  KC_H, KC_J, KC_K,    KC_L,   LT(MOVE, KC_SCLN), KC_NO,
-    KC_NO, LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_BSPC, KC_VOLU,     KC_BRIU, KC_NO,   KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH),   KC_NO,
-        LOPT_T(KC_ESC), LCMD_T(KC_BSPC),    HYPR(KC_NO),  KC_VOLD,     KC_BRID, LCTL_T(KC_ENT),    LT(SYMB, KC_SPC), ROPT_T(KC_TAB)
+    KC_TAB,         KC_Q,         KC_W, KC_E, KC_R, KC_T,                                        KC_Y, KC_U, KC_I,    KC_O,   KC_P,              KC_BSLS,
+    LCTL_T(KC_ESC), LCTL_T(KC_A), KC_S, KC_D, KC_F, KC_G, KC_ESC,                        KC_SPC, KC_H, KC_J, KC_K,    KC_L,   LT(MOVE, KC_SCLN), KC_QUOT,
+    KC_LSFT,        LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_BSPC, KC_VOLU,     KC_BRIU, KC_TAB, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH),   KC_ENT,
+                 LOPT_T(KC_ESC), LCMD_T(KC_BSPC),    HYPR(KC_NO),  KC_VOLD,     KC_BRID, LCTL_T(KC_ENT),    LT(SYMB, KC_SPC), ROPT_T(KC_TAB)
     ),
 /* Keymap 1: Symbols layer
  *
  * ,-------------------------------------------.                         ,-------------------------------------------.
- * |        |  1!  |  2@  |  3#  |  4$  |  5%  |                         |  6^  |  7&  |  8*  |  9(  |  0)  |        |
+ * |   `~   |  1!  |  2@  |  3#  |  4$  |  5%  |                         |  6^  |  7&  |  8*  |  9(  |  0)  |        |
  * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
  * |        |Ctl/F1|  F2  |  F3  |  F4  |  F5  |  F6  |           |      |      |  - _ |  = + |  `~  |  ' " |        |
  * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------'                     `--------------'
  */
 [SYMB] = LAYOUT_gergo(
-    KC_TRNS, KC_1,          KC_2,  KC_3,  KC_4,   KC_5,                                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            KC_TRNS,
+    KC_GRV,  KC_1,          KC_2,  KC_3,  KC_4,   KC_5,                                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            KC_TRNS,
     KC_TRNS, LCTL_T(KC_F1), KC_F2, KC_F3, KC_F4,  KC_F5,   KC_F6,                            KC_TRNS, KC_TRNS, KC_MINS, KC_EQL,  KC_GRV,  KC_QUOT,         KC_TRNS,
     KC_TRNS, LSFT_T(KC_F7), KC_F8, KC_F9, KC_F10, KC_F11,  KC_F12,  KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, RSFT_T(KC_PIPE), KC_TRNS,
                                           KC_TRNS, KC_TRNS, KC_SCLN, KC_EQL,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
