7afc413d828a3d5d7133fdeeba18c92c1e783c77
Peter Jang <peter.x.jang@gmail.com>
2020-11-19 21:17:34 -0600

    Adjust keys

--- keyboards/gergo/keymaps/peterxjang/keymap.c
+++ keyboards/gergo/keymaps/peterxjang/keymap.c
@@ -45,13 +45,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |Shft/Z|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  | ,  < | . >  |Shft/?|        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *                       .-------.   .------.                                 .------.   .-------.
- *                       |Opt/Tab|   |Cmd/Bs|                                 |L1/Spc|   |Ctr/Tab|
+ *                       |Opt/Esc|   |Cmd/Bs|                                 |L1/Spc|   |Ctr/Bs |
  *                       '-------'   '------'                                 `------.   '-------'
  *                                        ,-------.                      ,-------.
  *                                        |       |                      |       |
  *                                 ,------|-------|                      |-------|------.
  *                                 | HYPR |       |                      |       | Opt  |
- *                                 | Esc  |       |                      |       | Enter|
+ *                                 | Tab  |       |                      |       | Enter|
  *                                 |      |       |                      |       |      |
  *                                 `--------------'                      `--------------'
  */
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO, KC_Q,         KC_W, KC_E, KC_R, KC_T,                                 KC_Y, KC_U, KC_I,    KC_O,   KC_P,              KC_NO,
     KC_NO, LCTL_T(KC_A), KC_S, KC_D, KC_F, KC_G, KC_NO,                   KC_NO, KC_H, KC_J, KC_K,    KC_L,   LT(MOVE, KC_SCLN), KC_NO,
     KC_NO, LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_NO, KC_NO,     KC_NO, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH),   KC_NO,
-       LOPT_T(KC_TAB), LCMD_T(KC_BSPC), HYPR_T(KC_ESC), KC_NO,     KC_NO, ROPT_T(KC_ENT), LT(SYMB, KC_SPC), RCTL_T(KC_TAB)
+       LOPT_T(KC_ESC), LCMD_T(KC_BSPC), HYPR_T(KC_TAB), KC_NO,     KC_NO, ROPT_T(KC_ENT), LT(SYMB, KC_SPC), RCTL_T(KC_BSPC)
     ),
 /* Keymap 1: Symbols layer
  *
