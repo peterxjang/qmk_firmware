3922e5caa2df69b0e97d177117dd08512b63d96c
Peter Jang <peter.x.jang@gmail.com>
2021-01-29 20:54:25 -0600

    Swap comma and semicolon

--- keyboards/gergo/keymaps/peterxjang/keymap.c
+++ keyboards/gergo/keymaps/peterxjang/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                         ,-------------------------------------------.
  * |        |   Q  |   W  |   E  |   R  |   T  |                         |   Y  |   U  |   I  |   O  |   P  |        |
  * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |        |   A  |   S  |   D  |   F  |   G  |      |           |      |   H  |   J  |   K  |   L  | ;/L2 |        |
+ * |        |   A  |   S  |   D  |   F  |   G  |      |           |      |   H  |   J  |   K  |   L  | '/L2 |        |
  * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
  * |        |Shft/Z|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  | ,  < | . >  |Shft/?|        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_gergo(
     KC_NO, KC_Q,         KC_W, KC_E, KC_R, KC_T,                                 KC_Y, KC_U, KC_I,    KC_O,   KC_P,              KC_NO,
-    KC_NO, KC_A,         KC_S, KC_D, KC_F, KC_G, KC_NO,                   KC_NO, KC_H, KC_J, KC_K,    KC_L,   LT(MOVE, KC_SCLN), KC_NO,
+    KC_NO, KC_A,         KC_S, KC_D, KC_F, KC_G, KC_NO,                   KC_NO, KC_H, KC_J, KC_K,    KC_L,   LT(MOVE, KC_QUOT), KC_NO,
     KC_NO, LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_NO, KC_NO,     KC_NO, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH),   KC_NO,
        LOPT_T(KC_ESC), LCMD_T(KC_BSPC), LCTL_T(KC_TAB), KC_NO,     KC_NO, HYPR_T(KC_ENT), LT(SYMB, KC_SPC), KC_BSPC
     ),
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                         ,-------------------------------------------.
  * |        |  1!  |  2@  |  3#  |  4$  |  5%  |                         |  6^  |  7&  |  8*  |  9(  |  0)  |        |
  * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  - _ |  = + |  `~  |  ' " |        |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  - _ |  = + |  `~  |  ; : |        |
  * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
  * |        |Shf/F7|  F8  |  F9  |  F10 |  F11 |      |           |      |  F12 |      |  [{  |  ]}  |Shf |\|        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMB] = LAYOUT_gergo(
     KC_TRNS, KC_1,          KC_2,  KC_3,  KC_4,   KC_5,                                               KC_6,   KC_7,    KC_8,    KC_9,    KC_0,            KC_TRNS,
-    KC_TRNS, KC_F1,         KC_F2, KC_F3, KC_F4,  KC_F5,   KC_TRNS,                          KC_TRNS, KC_F6,  KC_MINS, KC_EQL,  KC_GRV,  KC_QUOT,         KC_TRNS,
+    KC_TRNS, KC_F1,         KC_F2, KC_F3, KC_F4,  KC_F5,   KC_TRNS,                          KC_TRNS, KC_F6,  KC_MINS, KC_EQL,  KC_GRV,  KC_SCLN,         KC_TRNS,
     KC_TRNS, LSFT_T(KC_F7), KC_F8, KC_F9, KC_F10, KC_F11,  KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_F12, KC_TRNS, KC_LBRC, KC_RBRC, RSFT_T(KC_BSLS), KC_TRNS,
                                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
